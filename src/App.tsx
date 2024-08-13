5eb41b22a77ea9481d813325495d23953c72700f
LucianoFlores <lucianof603@gmail.com>
2024-08-13 16:36:23 -0300

    Primer commit. Proyecto Base

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className="py-10 px-10 bg-gray-300 h-screen ">
+      <h1 className="font-bold text-3xl text-center">Holaaaaaaaaaa</h1>
+    </main>
+  )
+}
+
+export default App
