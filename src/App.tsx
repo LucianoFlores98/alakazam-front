4fbc0c6c0980465304953debaeb22a38beda044c
LucianoFlores <lucianof603@gmail.com>
2024-08-15 10:25:53 -0300

    Add dockerization with hot reload config & environments

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ function App() {
 
   return (
     <main className="py-10 px-10 bg-gray-300 h-screen ">
-      <h1 className="font-bold text-3xl text-center">Holaaaaaaaaaa</h1>
+      <h1 className="font-bold text-3xl text-center">Holaaaaaaaaaa. Funciona el hot reaload?</h1>
+      <h2> SI Vamoooooooooooooooooooooooo</h2>
     </main>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+   port: 3000,
+   strictPort: true,
+  },
+  server: {
+   port: 3000,
+   strictPort: true,
+   host: true,
+   watch:{
+    usePolling: true
+   }
+  },
+  build: { 
+    // Para resolver la advertencia del chunking al buildear
+    chunkSizeWarningLimit: 1600, 
+    rollupOptions: {
+    output:{
+        manualChunks(id) {
+            if (id.includes('node_modules')) {
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+        }
+    }
+} }
 })
