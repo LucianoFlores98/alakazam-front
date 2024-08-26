1f2345a52c2c397fb2f613cbb96446b5d9ed66e7
LucianoFlores <lucianof603@gmail.com>
2024-08-26 16:00:11 -0300

    Add material Symbols globally & types/react downgraded

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import FavIcon from "./components/FavIcon"
 import { Button } from "@material-tailwind/react";
 
+
 function App() {
 
   return (
     <main className="py-10 px-10 bg-gray-300 h-screen font-primaryRegular">
       <FavIcon />
       <Button className="bg-success ">Probando Material UI</Button>
+      <span className="material-symbols-rounded">home</span> 
 
     </main>
   )
