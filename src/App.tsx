61cd4a09150e88ae0296bd87ab2968127c9e4a15
LucianoFlores <lucianof603@gmail.com>
2024-08-25 21:45:20 -0300

    Fonts material taiwind fixed & add Poppins-light

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <main className="py-10 px-10 bg-gray-300 h-screen font-primaryRegular">
       <FavIcon />
       <Button className="bg-success ">Probando Material UI</Button>
+
     </main>
   )
 }
