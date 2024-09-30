06721ef29d920a238bb8f97648e6df9d56b14bbc
JuanIgnacioBr98 <bragajuanignacio803@gmail.com>
2024-09-30 14:31:54 -0300

    Add component UserBanner

--- src/components/UserBanner.tsx
+++ src/components/UserBanner.tsx
@@ -1,12 +1,10 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    Typography,
-    Chip,
-    Avatar,
-    Button,
-    Dialog,
+  Card,
+  Typography,
+  Chip,
+  Avatar,
+  Button,
+  Dialog,
 } from "@material-tailwind/react";
 import { currentUserRatingDummyData } from "../data/currentUserRating";
 import { userRatingDummyData } from "../data/userRatings";
@@ -14,79 +12,119 @@ import { UserRating } from "./UserRating";
 import React from "react";
 
 interface Props {
-    userName: string;
-    isVerified: boolean;
-    userLocation: string;
-    userImage: string;
-    userTag: {
-        idTag: string;
-        nameTag: string;
-    };
+  userName: string;
+  isVerified: boolean;
+  userLocation: string;
+  userImage: string;
+  userTag: {
+    idTag: string;
+    nameTag: string;
+  };
 }
 
+const getVerificationIcon = (isVerified: boolean) =>
+  isVerified ? "verified" : "brightness_empty";
+
 const UserBanner: React.FC<Props> = ({
-    userName,
-    isVerified,
-    userImage,
-    userTag,
+  userName,
+  isVerified,
+  userLocation,
+  userImage,
 }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(!open);
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(!open);
+  return (
+    <div className="flex justify-center">
+      <Card className="lg:w-auto sm:w-[20rem] p-4">
+        <div className=" flex flex-col lg:flex-row gap-3 lg:items-center w-auto">
+          <div className="text-center">
+            <Button
+              onClick={handleOpen}
+              className="rounded-full p-0.5 bg-transparent"
+            >
+              <Avatar
+                size="xxl"
+                src={userImage}
+                alt={"Foto de perfil de " + userName}
+                className="ring-2 ring-info p-0.5 lg:size-36"
+              />
+            </Button>
+            <Dialog open={open} handler={handleOpen} size="xs">
+              <img
+                alt="nature"
+                className="h-full w-full rounded-lg object-cover object-center"
+                src={userImage}
+              />
+            </Dialog>
+          </div>
 
-    return (
-        <div className="flex justify-center">
-            <Card className="lg:w-auto sm:w-[20rem] p-4">
-                <div className=" flex flex-col lg:flex-row gap-3 lg:items-center w-auto">
-                    <div className="text-center">
-                        <Button onClick={handleOpen} className="rounded-full p-0.5 bg-transparent">
-                            <Avatar
-                                size="xxl"
-                                src={userImage}
-                                className="ring-2 ring-blue-700 p-0.5 lg:size-36"
-                            />
-                        </Button>
-                        <Dialog open={open} handler={handleOpen} size="xs">
-                            <img
-                                alt="nature"
-                                className="h-full w-full rounded-lg object-cover object-center"
-                                src={userImage}
-                            />
-                        </Dialog>
-                    </div>
+          {/* Información del usuario */}
+          <div className="text-left max-w-72 lg:max-w-96">
+            <Typography className="text-2xl lg:text-3xl font-bold text-gray-900 inline">
+              {userName}
+              <span
+                className={`material-symbols-rounded ${isVerified ? "text-info" : "text-nico"
+                  } align-middle ml-2`}
+              >
+                {getVerificationIcon(isVerified)}
+              </span>
+            </Typography>
 
-                    {/* Información del usuario */}
-                    <div className="text-left lg:ml-4">
-                        <Typography className=" text-2xl lg:text-3xl font-bold text-gray-900">
-                            {userName}
-                        </Typography>
-                        {isVerified ? (
-                            <Typography className="text-info flex gap-2" >Cliente verificado <span className="material-symbols-rounded">verified</span></Typography>
-                        ) : (
-                            <Typography className="text-danger">Cliente no verificado :C</Typography>
-                        )}
+            <Typography className="text-xs text-secondary lg:text-sm flex items-center mt-1">
+              <span className="material-symbols-rounded">location_on</span>
+              {userLocation}
+            </Typography>
 
-                        <div className="flex flex-wrap max-w-[17rem] gap-1 py-2 mt-3">
-                            <Chip variant="filled" className="bg-info" value="Capo" />
-                            <Chip variant="outlined" className="text-info border-info" value="Idolo" />
-                            <Chip variant="ghost" className="bg-info bg-opacity-20 text-info" value="Master" />
-                            <Chip variant="gradient" color="blue" value="Lince" />
-                            <Chip variant="outlined" className="text-info border-info" value="Ete se sale" />
-                        </div>
-                    </div>
+            <div className="flex flex-wrap max-w-[18rem] lg:max-w-80 gap-1 mt-2">
+              {/* {isVerified && (
+                <Chip
+                  variant="gradient"
+                  className="bg-gradient-to-tr from-green-400 to-green-600"
+                  value="Usuario Verificado"
+                  size="sm"                
+                />
+              )} */}
+              <Chip
+                variant="filled"
+                className="bg-info"
+                value="Amable"
+                size="sm"
+              />
+              <Chip
+                variant="outlined"
+                className="text-info border-info"
+                value="Se ocupa"
+                size="sm"
+              />
+              <Chip
+                variant="ghost"
+                className="bg-info bg-opacity-20 text-info"
+                value="Confiable"
+                size="sm"
+              />
+              <Chip
+                variant="gradient"
+                className="bg-gradient-to-tr from-light-blue-300 to-info"
+                value="Volveria a alquilar"
+                size="sm"
+              />
+            </div>
+          </div>
 
-                    {/* Ratings centrados a la derecha */}
-                    <div className="lg:w-auto lg:flex lg:justify-center lg:items-center">
-                        <UserRating
-                            userRatings={userRatingDummyData}
-                            currentUserRating={currentUserRatingDummyData}
-                        />
-                    </div>
-                </div>
-            </Card>
+          <div className="flex-col flex lg:w-auto items-center">
+            <UserRating
+              userRatings={userRatingDummyData}
+              currentUserRating={currentUserRatingDummyData}
+            />
+            <Typography className="text-center text-info hover:underline mt-2 text-sm">
+              <a href="#">Ver opiniones</a>
+            </Typography>
+          </div>
         </div>
-    );
+      </Card>
+    </div>
+  );
 };
-
 
 export default UserBanner;
