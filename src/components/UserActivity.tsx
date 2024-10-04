5d4fc237298ae2e7c71c2f6e5e10a1bdedbfbf61
Juan Augusto Haser <juanaugustohaser@gmail.com>
2024-10-04 16:13:31 -0300

    feature: userActivity

--- src/components/UserActivity.tsx
+++ src/components/UserActivity.tsx
@@ -1,44 +1,40 @@
-import { Component } from 'react';
+const UserActivity: React.FC = () => {
 
-export class UserActivity extends Component {
-  static propTypes = {}
+  const activities = [
+    { fecha: '14/7', actividad: 'Bajó el precio de su alquiler.', link: 'Ver publicación' },
+    { fecha: '14/7', actividad: 'Lo contactó Marcos Galperin por un alquiler.', link: 'Ver alquiler' },
+    { fecha: '14/7', actividad: 'Opinó sobre un inquilino.', link: 'Ver opinión' },
+    { fecha: '13/7', actividad: 'Recibió una opinión de un inquilino.', link: 'Ver opinión' },
+    { fecha: '13/7', actividad: 'Publicó un nuevo inmueble.', link: 'Ver publicación' },
+    { fecha: '13/7', actividad: 'Por buen historial recibió un sticker por amabilidad.' },
+  ];
 
-  render() {
-    const activities = [
-      { fecha: '14/7', actividad: 'Bajó el precio de su alquiler.', link: 'Ver publicación' },
-      { fecha: '14/7', actividad: 'Lo contactó Marcos Galperin por un alquiler.', link: 'Ver alquiler' },
-      { fecha: '14/7', actividad: 'Opinó sobre un inquilino.', link: 'Ver opinión' },
-      { fecha: '13/7', actividad: 'Recibió una opinión de un inquilino.', link: 'Ver opinión' },
-      { fecha: '13/7', actividad: 'Publicó un nuevo inmueble.', link: 'Ver publicación' },
-      { fecha: '13/7', actividad: 'Por buen historial recibió un sticker por amabilidad.' },
-    ];
+  return (
+    <div className='p-4 max-w-xl'>
 
-    return (
-      <div className='p-4 max-w-xl'>
-
-        <div className='flex justify-between items-center mb-4'>
-          <h4 className='text-red-400 text-lg font-semibold'>Actividad reciente</h4>
-          <button className='text-red-400 text-sm font-medium'>Ordenar por: Más nuevos</button>
-        </div>
-        
-        <div className='border border-red-200 rounded-lg px-2 py-2'>
-          {activities.map((item, index) => (
-            <div key={index} className='mb-2'>
-
-              <span className='text-gray-600 font-medium mr-2'>{item.fecha}</span>
-              <span className='text-gray-800'>{item.actividad} </span>
-
-              {item.link && (
-                <a href="#" className='text-blue-500 hover:underline'>
-                  {item.link}
-                </a>
-              )}
-            </div>
-          ))}
-        </div>
+      <div className='flex justify-between items-center mb-4'>
+        <h4 className='text-red-400 text-lg font-semibold'>Actividad reciente</h4>
+        <button className='text-red-400 text-sm font-medium'>Ordenar por: Más nuevos</button>
       </div>
-    );
-  }
+      
+      <div className='border border-red-200 rounded-lg px-2 py-2'>
+        {activities.map((item, index) => (
+          <div key={index} className='mb-2'>
+
+            <span className='text-gray-600 font-medium mr-2'>{item.fecha}</span>
+            <span className='text-gray-800'>{item.actividad} </span>
+
+            {item.link && (
+              <a href="#" className='text-blue-500 hover:underline'>
+                {item.link}
+              </a>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
 
 export default UserActivity;
